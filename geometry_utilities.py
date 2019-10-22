d480965fe3ac6777471bd00d73e77586844c38a5
HEllicott <hannah.ellicott@os.uk>
2019-10-22 18:00:15 +0100

    geo utils intersection tests and code working

--- geometry_utilities.py
+++ geometry_utilities.py
@@ -1,4 +1,4 @@
-from shapely.geometry.point import Point
+from shapely.geometry import Point, Polygon
 
 
 class GeoUtils(object):
@@ -6,3 +6,6 @@ class GeoUtils(object):
     def circle(self, centre_point: Point, radius):
         circle = centre_point.buffer(radius)
         return circle
+
+    def point_in_polygon(self, point: Point, poly: Polygon):
+        return poly.contains(point)
--- unittests/test_geometry_utilities.py
+++ unittests/test_geometry_utilities.py
@@ -14,3 +14,23 @@ class TestGeoUtils(TestCase):
         circle_to_test = geo_utils.circle(centre_point, radius)
         # assert
         assert circle_to_test.centroid == centre_point
+
+    def test_point_in_polygon_returns_true_when_point_inside_polygon(self):
+        # arrange
+        geo_utils = GeoUtils()
+        poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+        point = Point(0.4, 0.4)
+        # act
+        result = geo_utils.point_in_polygon(point, poly)
+        # assert
+        assert result is True
+
+    def test_point_in_polygon_returns_false_when_point_inside_polygon(self):
+        # arrange
+        geo_utils = GeoUtils()
+        poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+        point = Point(0.4, 1.4)
+        # act
+        result = geo_utils.point_in_polygon(point, poly)
+        # assert
+        assert result is False
