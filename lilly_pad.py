12615a8e8c064ffb9f68be630f088c45218928b4
HEllicott <hannah.ellicott@os.uk>
2019-10-23 09:52:38 +0100

    lilly pad polygon test and code working

--- lilly_pad.py
+++ lilly_pad.py
@@ -1,10 +1,16 @@
 import random
 
+from geometry_utilities import GeoUtils
+
 
 class LillyPad(object):
 
-    def __init__(self, position,min_radius, max_radius):
+    def __init__(self, position, min_radius, max_radius):
         pass
 
     def _choose_radius(self, min_radius, max_radius):
         return random.uniform(min_radius, max_radius)
+
+    def _create_polygon(self, position, radius):
+        geo_utils = GeoUtils()
+        return geo_utils.circle(position, radius)
--- unittests/test_lilly_pad.py
+++ unittests/test_lilly_pad.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from shapely.geometry.point import Point
+from shapely.geometry import Point, MultiPoint
 
 from lilly_pad import LillyPad
 
@@ -16,3 +16,15 @@ class TestLillyPad(TestCase):
         radius = pad._choose_radius(min_rad, max_rad)
         # assert
         assert min_rad < radius < max_rad
+
+    def test_calculate_polygon_returns_correct_size_polygon(self):
+        # arrange
+        centre = Point(1, 1)
+        max_rad = 5
+        min_rad = 2
+        pad = LillyPad(centre, min_rad, max_rad)
+        # act
+        poly = pad._create_polygon(centre, 3)
+        # assert
+        poly_point = MultiPoint(poly.exterior.coords)[0]
+        assert 2.9 < centre.distance(poly_point) < 3.1
