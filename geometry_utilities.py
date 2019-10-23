31f978c308c8a6ecc6b281e33792f27d23c6b76a
HEllicott <hannah.ellicott@os.uk>
2019-10-23 10:24:20 +0100

    added circle intersect test and method

--- geometry_utilities.py
+++ geometry_utilities.py
@@ -7,6 +7,11 @@ class Circle(object):
         self.centre_point = centre_point
         self.radius = radius
 
-    def contains_point(self, other_point):
+    def contains_point(self, other_point: Point):
         distance = self.centre_point.distance(other_point)
         return distance <= self.radius
+
+    def intersects_circle(self, other_circle):
+        distance = self.centre_point.distance(other_circle.centre_point)
+        max_distance = self.radius + other_circle.radius
+        return distance <= max_distance
--- unittests/test_geometry_utilities.py
+++ unittests/test_geometry_utilities.py
@@ -26,6 +26,7 @@ class TestCircle(TestCase):
         assert result is False
 
     def test_contains_point_returns_true_when_point_on_boundary_of_circle(self):
+        # arrange
         centre = Point(3, 3)
         circle = Circle(centre, 2)
         point_outside_circle = Point(3, 1)
@@ -33,3 +34,21 @@ class TestCircle(TestCase):
         result = circle.contains_point(point_outside_circle)
         # assert
         assert result is True
+
+    def test_intersects_circle_returns_true_when_circles_overlap(self):
+        # arrange
+        circle1 = Circle(Point(3, 3), 1)
+        circle2 = Circle(Point(2, 1), 2)
+        # act
+        result = circle1.intersects_circle(circle2)
+        # assert
+        assert result is True
+
+    def test_intersects_circle_returns_false_when_circles_do_not_overlap(self):
+        # arrange
+        circle1 = Circle(Point(10, 10), 1)
+        circle2 = Circle(Point(2, 1), 2)
+        # act
+        result = circle1.intersects_circle(circle2)
+        # assert
+        assert result is False
