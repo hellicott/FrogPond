b6c4e6be6f1066aaa1ecb3bbf380286196ecb6f2
HEllicott <hannah.ellicott@os.uk>
2019-10-25 16:10:37 +0100

    draws circle

--- visualise.py
+++ visualise.py
@@ -1,4 +1,5 @@
 from geometry_utilities import Circle
+from shapely.geometry.point import Point
 
 from tkinter import Tk, Canvas
 
@@ -8,12 +9,12 @@ class Visualise(object):
     def __init__(self, size):
         master = Tk()
         self.canvas = Canvas(master, width=size, height=size)
+        self.draw_circle(Circle(Point(100, 100), 25))
         self.canvas.pack()
         master.mainloop()
 
-
-    def draw_circle(self, circle: Circle):
-        self.canvas.create_oval(circle.)
+    def draw_circle(self, circle: Circle, colour="red"):
+        self.canvas.create_oval(circle.get_bounds(), fill=colour)
 
 
 Visualise(300)
